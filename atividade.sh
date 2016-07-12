eac81c35ba1ba435764abe5d93490fed01790fc5
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2016-07-12 02:55:34 -0300

    Fim da atividade

--- atividade.sh
+++ atividade.sh
@@ -8,13 +8,18 @@ wget http://ita.ee.lbl.gov/traces/calgary_access_log.gz -O acessfile.gz
 gunzip acessfile.gz
 
 # remove todas as linhas que nao tenham o padrao especificado
-# -i in-plase, altera diretamente no arquivo
+# -i in-place, altera diretamente no arquivo
 # !d remove as linas
 sed -i '/ - - /!d' acessfile
 
 # variaveis para contabilizar os arquivos remote e local
 remotes=0;
 locals=0;
+horas_remotes=0
+horas_locals=0
+
+# ISF (internal field separator) recebendo um novo separador
+IFS=":"
 
 # iterando sobre as linhas do arquivo
 while read line
@@ -22,17 +27,35 @@ do
 	# verifica se a linha contem a seguinte expressao e incrementa a quatidae de acessos
         if [[ $line == *"remote"* ]]
         then
+                #atribuicao de valores juntamente com operacaoa  aritimetica
                 let remotes=$remotes+1;
+		set -- $line
+                # eh usado o 10#$2 para informar que o valor de $2 sera decimal. base 1o
+                # caso nao seja feito isso lancara um erro por uso de octais
+		let horas_remotes=$horas_remotes+10#$2;
         elif [[ $line == *"local"* ]]
         then
                 let locals=$locals+1;
+                let horas_locals=$horas_locals+10#$2;
+
         fi
 done < acessfile
 
 echo "Total de requisicoes locais $locals"
 echo "Total de requisicoes remotas $remotes"
+echo "Total de horas remotes $horas_remotes"
+echo "Total de horas locals $horas_locals"
+echo "Media de horas em que as requisicoes remotas sao feitas $(($horas_remotes/$remotes))"
+echo "Media de horas em que as requisicoes locais sao feiras $(($horas_locals/$locals))"
 
 
-
-
+if [[ $locals -gt $remotes ]];
+then
+        echo "Os arquivos mais acessados sao os locals"
+elif [[ $remotes -gt $locals ]];
+then 
+        echo "Os arquivos mais acessados sao os remotes"
+else
+        echo "Os arquivos tem a mesma quantidade de acessos"
+fi
 
