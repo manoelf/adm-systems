62c9503ba995ebe008cc3c5f894c9205a0ded67d
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2016-09-20 15:39:07 -0300

    atividae 12 em desenvolvimento

--- atividade12/atividade12.sh
+++ atividade12/atividade12.sh
@@ -7,9 +7,8 @@ function rename_folder {
 
     if [ -d "$1" ];then
         version=$(echo $1 | cut -d "." -f2)
-        file_name=$(echo $1 | cut -d "." -f1)
+        file_name=$(echo $1 | cut -d "." -f1-)
         if [ "$1" == "$version" ];then
-            #version=$(($version+1))
             rename_folder "$file_name.1"
         else
             version=$(($version+1))
@@ -19,10 +18,46 @@ function rename_folder {
         echo "$1"
     fi
 }
-var=$(echo $1 | rev | cut -d "/" -f2 | rev)
-echo $var
-FOLDER_NAME=$(rename_folder "$2$var")
-echo $FOLDER_NAME
-#rename_folder $1
-cp -R $1 $FOLDER_NAME
 
+
+
+
+
+function rename_file {
+	if [ -e $1 ];then
+		version=$(echo $1 | rev | cut -d "." -f1 | rev)
+		file_name=$(echo $1 | rev | cut -d "." -f2- | rev)
+
+		if [ "$1" == "$version" ];then 
+			rename_file  "$file_name.$version.1"
+		else 
+			version=$(($version+1))
+			rename_file "$file_name.$version"
+		fi
+	else 
+		echo "$1"
+	fi
+}
+
+#cp -R $1 $FOLDER_NAME
+
+if [ -f "$1" ];then 
+	var=$(echo "$1" | rev | cut -d "/" -f1 | rev)
+	FOLDER_NAME=$(rename_file "$2$var")
+	echo $var
+	echo $FOLDER_NAME
+	echo "if"
+elif [ -d "$1" ];then
+	var=$(echo $1 | rev | cut -d "/" -f2 | rev)
+	FOLDER_NAME=$(rename_folder "$2$var")
+	echo $var
+	echo $FOLDER_NAME
+	echo "elif"
+else
+	echo "ERRADO!"
+fi
+
+cp -R $1 $FOLDER_NAME
+#fodlName=$(rename_file "$2$var")
+#echo $var
+#echo $fodlName
